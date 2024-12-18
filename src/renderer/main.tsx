b007a5afe006fbd5ad0532995b84fd1ba4b83cf9
Alberto Llamocca <proveedor66@culqi.com>
2024-12-18 07:43:32 +0000

    feat: init project

---
+++ src/renderer/main.tsx
@@ -0,0 +1,7 @@
+import "./styles/global.css";
+
+import { createRoot } from "react-dom/client";
+import { MainPage } from "./pages";
+
+const root = createRoot(document.body);
+root.render(<MainPage />);
